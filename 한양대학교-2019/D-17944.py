f8e07b8b84ef4dcab7cb35ad2ce8c352aad93b1a
HAIL <s000715@naver.com>
2020-05-17 16:55:47 +0900

    Chung-ang Univ 2019

--- 한양대학교-2019/D-17944.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-n,t=map(int, sys.stdin.readline().split())
-
-nam=(t%(4*n-2))
-
-if nam>2*n: nam=4*n-nam
-sys.stdout.write(str(nam))
--- 한양대학교-2019/I-17950.py
+++
@@ -1,9 +0,0 @@
-import sys
-height, mul=map(int, sys.stdin.readline().split())
-temp,result=1,0
-for _ in range(height):
-    temp*=mul
-    temp%=1000000007
-    result+=int(sys.stdin.readline())*temp
-    result%=1000000007
-sys.stdout.write(str(result))
